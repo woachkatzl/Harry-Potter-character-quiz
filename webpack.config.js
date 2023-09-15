e8d8be3a8743f45ff49a1008931b0cff743fe0fc
woachkatzl <y.v.polonskaya@gmail.com>
2023-09-15 15:01:26 -0400

    project npm structure base

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path'); // подключение стандартной библиотеки Node.js - path, что-то вроде разборщика путей
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js', // точка входа
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(__dirname, './public/index.html'),
+        }),
+    ],
+    output: {                // указание, куда собирать проект
+        filename: 'main.js',   // файл, который генерируется и передается в браузер
+        path: path.resolve(__dirname, 'bundle'), // название директория
+    },
+};
