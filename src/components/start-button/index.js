305a694ed873d02c311a7fdf530218538003ac09
woachkatzl <y.v.polonskaya@gmail.com>
2023-09-18 15:48:45 -0400

    example button component

---
+++ src/components/start-button/index.js
@@ -0,0 +1,24 @@
+import './styles.scss';
+
+class Button {
+    constructor(onClick, text) {
+        this.onClick = onClick;
+        this.text = text;
+    }
+
+    onClickHandler = () => {
+        this.onClick();
+    }
+
+    render() {
+        const button = document.createElement("button");
+
+        addEventListener("click", this.onClickHandler);
+        button.textContent = this.text;
+        button.classList.add("start-button");
+
+        return button;
+    }
+}
+
+export { Button };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './assets/styles/global.scss';
+import { Button } from './components/start-button';
+
+function magic() {
+    console.log("Success!");
+}
+
+const startButton = new Button(magic, "START");
+
+document.body.append(startButton.render());
