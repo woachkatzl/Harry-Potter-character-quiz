348fde05ae1b06a866c76dcaf5566fd09f3a9897
woachkatzl <y.v.polonskaya@gmail.com>
2023-09-20 00:17:31 -0400

    styled landing page, added more colors and mixins

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import './assets/styles/global.scss';
-import { Button } from './components/start-button';
-
-function magic() {
-    console.log("Success!");
-}
-
-const startButton = new Button(magic, "START");
-
-document.body.append(startButton.render());
