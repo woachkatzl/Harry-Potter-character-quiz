5455ea0f683370f2c92c1e51c09b38a33f8cf38b
your-m1nd <vi.neurogirl@gmail.com>
2023-09-21 22:03:25 +0300

    add ui-kit text component

--- src/presentation/ui-kit/text/index.js
+++ src/presentation/ui-kit/text/index.js
@@ -1,1 +1,20 @@
 //Здесь будет класс, создающий текст в теге <span>, который можно вставлять в другие элементы.
+import "./styles.scss";
+
+class Text {
+
+  constructor(text) {
+    this.text = text;
+  }
+
+  render = () => {
+    const span = document.createElement("span");
+
+    span.textContent = this.text;
+    span.className = "";
+
+    return span;
+  };
+}
+
+export { Text };
