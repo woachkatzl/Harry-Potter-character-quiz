fa240f29606d3c73379489deae7d28b42bab5e41
your-m1nd <vi.neurogirl@gmail.com>
2023-09-23 21:41:02 +0300

    try to create list at infobox

--- src/domain/entity/Character.js
+++ src/domain/entity/Character.js
@@ -3,12 +3,22 @@
 class Character {
   #name;
   #image;
+  #house;
+  #species;
+  #ancestry;
+  #yearOfBirth;
+  #actor;
 
   //В конструктор добавляем все свойства объекта, которые хотим выводит в результатах. Также добавляем их выше с # и ниже в геттеры
-  constructor(name, image) {
+  constructor(name, image, house, species, ancestry, yearOfBirth, actor) {
     this.#name = name;
     this.#image = image;
-  }
+    this.#house = house;
+    this.#species = species;
+    this.#ancestry = ancestry;
+    this.#yearOfBirth = yearOfBirth;
+    this.#actor = actor;
+}
 
   get name() {
     return this.#name;
@@ -17,6 +27,27 @@ class Character {
   get image() {
     return this.#image;
   }
+
+  get house() {
+    return this.#house;
+  }
+
+  get species() {
+    return this.#species;
+  }
+
+  get ancestry() {
+    return this.#ancestry;
+  }
+
+  get yearOfBirth() {
+    return this.#yearOfBirth;
+  }
+
+  get actor() {
+    return this.#actor;
+  }
+
 }
 
 export { Character };
--- src/presentation/components/info-box/index.js
+++ src/presentation/components/info-box/index.js
@@ -1,15 +1,21 @@
 //Здесь будет класс InfoBox, на основе которого будет отрисовываться div-рамка с инофрмацией о персонаже
 import "./styles.scss";
+import { List } from "../../ui-kit/list";
 
 class InfoBox {
 
-  constructor(name) {
+  constructor(name, house, species, ancestry, yearOfBirth, actor) {
     //Сразу в конструкторе создаём див-обёртку и присваем ему класс со стилями. 
     this.wrapper = document.createElement("div");
     this.wrapper.className = "result";
 
     //Если хотим добавить информацию, выводимую в рамке, добавляем её как аргумент здесь в конструктор
     this.name = name;
+    this.house = house;
+    this.species = species;
+    this.ancestry = ancestry;
+    this.yearOfBirth = yearOfBirth;
+    this.actor = actor;
   }
 
   render() {
@@ -21,8 +27,18 @@ class InfoBox {
     title.className = "result__title";
     title.appendChild(new Text(this.name));
 
+    //Создаём список с категориями персонажа
+    //Создаём 2 массива для списков категорий
+    const categoriesList = [this.house, this.species, this.ancestry, this.yearOfBirth, this.actor];
+    const resultsList = ["test"];
+
+    const resultInfo = document.createElement("div");
+    resultInfo.className = "result__info";
+    resultInfo.appendChild(new List(categoriesList, resultsList));
+
     //Добавляем созданные элементы в див-обёртку
     this.wrapper.appendChild(title);
+    this.wrapper.appendChild(resultInfo);
 
     return this.wrapper;
   }
